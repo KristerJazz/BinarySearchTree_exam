38478a6a46d6de4e7e896731c052560afefa24be
Jazz <kristerjazz.urog@gmail.com>
2020-01-13 00:51:23 +0100

    Commented out some large number of nodes

--- job.sh
+++ job.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "N,straight,random,balance,map" >> benchmark_results/results.txt
 
-for i in 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072
+for i in 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 #65536 131072
 	do
+	echo ${i}
 	./bst.x ${i} >>  benchmark_results/results.txt
 	cp benchmark_results/results.txt results_so_far.txt
 	done
